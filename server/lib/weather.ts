322a8d98d406d77713231a5b19553b40c377fdf6
Zane Bauman <zanebauman55@gmail.com>
2025-05-21 20:05:23 -0400

    working toward animation graph

--- server/lib/weather.ts
+++ server/lib/weather.ts
@@ -1,121 +1,132 @@
 import z from "zod";
 
-const unitCodeSchema = z.string().transform((value) => value.replace(/^[^:]+:/, '').toLowerCase());
-const quantitativeValueSchema = z.object({
-  value: z.number().optional().nullable(),
-  maxValue: z.number().optional().nullable(),
-  minValue: z.number().optional().nullable(),
-  // "A string denoting a unit of measure, expressed in the format \"{unit}\" or \"{namespace}:{unit}\".
-  //Units with the namespace \"wmo\" or \"wmoUnit\" are defined in the World Meteorological Organization
-  // Codes Registry at http://codes.wmo.int/common/unit and should be canonically resolvable to
-  // http://codes.wmo.int/common/unit/{unit}. Units with the namespace \"nwsUnit\" are currently custom
-  // and do not align to any standard. Units with no namespace or the namespace \"uc\" are compliant
-  // with the Unified Code for Units of Measure syntax defined at https://unitsofmeasure.org/. This
-  // also aligns with recent versions of the Geographic Markup Language (GML) standard, the IWXXM
-  // standard, and OGC Observations and Measurements v2.0 (ISO/DIS 19156). Namespaced units are\
-  // considered deprecated. We will be aligning API to use the same standards as GML/IWXXM in
-  // the future.
-  unitCode: unitCodeSchema,
-})
+process.env.TZ = 'America/New_York'
+// https://open-meteo.com/en/docs
+// https://open-meteo.com/en/docs?latitude=40.037340675826286&longitude=-83.0197630420404&timezone=America%2FNew_York&forecast_days=3&hourly=temperature_2m,precipitation_probability,cloud_cover,wind_speed_10m,precipitation,wind_gusts_10m,uv_index,is_day&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&current=temperature_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min#data_sources
 
-/**
- * {
- *   "name": "Friday",
- *   "startTime": "2025-03-21T10:00:00.000Z",
- *   "endTime": "2025-03-21T22:00:00.000Z",
- *   "isDaytime": true,
- *   "temperature": 53,
- *   "shortForecast": "Sunny",
- *   "probabilityOfPrecipitation": {
- *     "value": null,
- *     "unitCode": "percent"
- *   },
- *   "windSpeed": {
- *     "maxValue": 22.224,
- *     "minValue": 5.556,
- *     "unitCode": "km_h-1"
- *   },
- *   "windDirection": "W"
- * }
- */
-const periodSchema = z.object({
-  /** Tonight, Thursday, Thursday Night, Wednesday, Wednesday Night */
-  name: z.string(),
-  startTime: z.string().transform((value) => new Date(value)),
-  endTime: z.string().transform((value) => new Date(value)),
-  isDaytime: z.boolean(),
-  /** converted to F */
-  temperature: quantitativeValueSchema.transform(({ unitCode, value }) => {
-    if (!value) { return 0; }
-    return /(deg)?C$/i.test(unitCode) ? (value * (9 / 5)) + 32 : value
+const endpoint = process.env.WEATHER_ENDPOINT;
+if (typeof endpoint !== 'string') {
+  throw new Error('No weather API endpoint set');
+}
+
+const weatherApiSchema = z.object({
+  utc_offset_seconds: z.number(),
+  timezone: z.string(),
+  timezone_abbreviation: z.string(),
+  // current_units: {
+  //   time: "unixtime",
+  //   interval: "seconds",
+  //   temperature_2m: "°F",
+  //   is_day: "",
+  //   precipitation: "inch",
+  //   cloud_cover: "%",
+  //   wind_speed_10m: "mp/h",
+  //   wind_direction_10m: "°",
+  //   wind_gusts_10m: "mp/h"
+  // },
+  current: z.object({
+    time: z.number().transform((t) => new Date(t * 1000)),
+    interval: z.number(),
+    temperature_2m: z.number(),
+    is_day: z.number().transform(Boolean),
+    precipitation: z.number(),
+    cloud_cover: z.number(),
+    wind_speed_10m: z.number(),
+    wind_direction_10m: z.number(),
+    wind_gusts_10m: z.number(),
   }),
-  /**
-   * "Chance Rain Showers then Partly Cloudy", "Chance Rain Showers", "Mostly Sunny then Slight Chance Rain Showers",
-   * "Chance Rain And Snow", "Mostly Cloudy then Slight Chance Rain Showers", "Sunny"
-   */
-  shortForecast: z.string(),
-  probabilityOfPrecipitation: quantitativeValueSchema.optional().nullable(),
-  windSpeed: quantitativeValueSchema.optional().nullable(),
-  windDirection: z.union([
-    z.literal("N"),
-    z.literal("NNE"),
-    z.literal("NE"),
-    z.literal("ENE"),
-    z.literal("E"),
-    z.literal("ESE"),
-    z.literal("SE"),
-    z.literal("SSE"),
-    z.literal("S"),
-    z.literal("SSW"),
-    z.literal("SW"),
-    z.literal("WSW"),
-    z.literal("W"),
-    z.literal("WNW"),
-    z.literal("NW"),
-    z.literal("NNW"),
-  ]),
+  // "hourly_units": {
+  //   "time": "unixtime",
+  //   "temperature_2m": "°F",
+  //   "precipitation_probability": "%",
+  //   "cloud_cover": "%",
+  //   "wind_speed_10m": "mp/h",
+  //   "precipitation": "inch",
+  //   "wind_gusts_10m": "mp/h",
+  //   "is_day": ""
+  // },
+  hourly: z.object({
+    time: z.number().transform((t) => new Date(t * 1000)).array(),
+    temperature_2m: z.number().array(),
+    precipitation_probability: z.number().array(),
+    cloud_cover: z.number().array(),
+    wind_speed_10m: z.number().array(),
+    precipitation: z.number().array(),
+    wind_gusts_10m: z.number().array(),
+    is_day: z.number().transform(Boolean).array(),
+  }),
+  // daily_units: {
+  //   "time": "unixtime",
+  //   "sunrise": "unixtime",
+  //   "sunset": "unixtime",
+  //   "temperature_2m_max": "°F",
+  //   "temperature_2m_min": "°F"
+  // },
+  daily: z.object({
+    time: z.number().transform((t) => new Date(t * 1000)).array(),
+    sunrise: z.number().transform((t) => new Date(t * 1000)).array(),
+    sunset: z.number().transform((t) => new Date(t * 1000)).array(),
+    temperature_2m_max: z.number().array(),
+    temperature_2m_min: z.number().array(),
+  })
 });
 
-const weatherDataResponseSchema = z.object({
-  periods: z.array(periodSchema),
-}).transform(({ periods }) => periods)
-export type WeatherDataResponse = z.infer<typeof weatherDataResponseSchema>
-
 export const getWeatherData = async () => {
-  const url = new URL(`https://api.weather.gov/gridpoints/ILN/84,84/forecast?units=us`);
-  const response = await fetch(url, {
-    headers: {
-      Accept: 'application/ld+json',
-      'Feature-Flags': 'forecast_temperature_qv,forecast_wind_speed_qv'
-    }
-  })
+  process.env.TZ = 'America/New_York'
+  const response = await fetch(endpoint);
 
   if (!response.ok) {
-    throw new Error(`Invalid response from ${url.host} - ${response.status}`)
+    throw new Error(`Bad weather API response "${response.status}".`)
   }
 
   const json = await response.json();
-  const parsedResponse = weatherDataResponseSchema.parse(json);
-  return parsedResponse;
+  const parsedJson = weatherApiSchema.parse(json);
+
+  const reformatted = {
+    ...parsedJson,
+    daily: {
+      time: parsedJson.daily.time[0]!,
+      sunrise: parsedJson.daily.sunrise[0]!,
+      sunset: parsedJson.daily.sunset[0]!,
+      temperature_2m_max: parsedJson.daily.temperature_2m_max[0]!,
+      temperature_2m_min: parsedJson.daily.temperature_2m_min[0]!,
+    }
+  }
+
+  return reformatted;
 }
 
+const groupValues = (input: number[]): number[] => {
+  const ret: number[] = []
+  for (let group = 0; group < 12; group++) {
+    let groupedValue = 0;
+    for (let value = 0; value < 2; value++) {
+      groupedValue += input[(group * 2) + value]!;
+    }
+    ret.push(groupedValue / 2);
+  }
 
-// const getWeatherOutput = async (commands: AllCommands[]) => {
-//   const wetherData = await getWeatherData();
-//   const currentWeather = wetherData[0];
-//   // const nextWeather = wetherData[1];
+  return ret;
+}
 
-//   if (currentWeather) {
-//     commands.push(createCommand({
-//       type: 'set-state',
-//       position: {
-//         x: 0,
-//         y: 32
-//       }
-//     }))
-//     commands.push(createCommand({
-//       type: 'string',
-//       value: `Temp ${currentWeather.temperature}F`,
-//     }))
-//   }
-// }
+export const generateGraphValues = ({
+  data,
+  defaultMax,
+  defaultMin,
+  graphHeight,
+}: {
+  data: number[],
+  defaultMax: number,
+  defaultMin: number,
+  graphHeight: number
+}) => {
+  const maxTemp = Math.max(data.reduce((acc, temp) => (temp > acc ? temp : acc), 0), defaultMax);
+  const minTemp = Math.min(data.reduce((acc, temp) => (temp < acc ? temp : acc), 1000), defaultMin);
+  const tempRange = maxTemp - minTemp;
+  const grouped = groupValues(data).map((temp) => {
+    const valP = (temp - minTemp) / tempRange;
+    return Math.round(valP * graphHeight);
+  })
+
+  return grouped
+}
--- server/main.ts
+++ server/main.ts
@@ -1,88 +1,123 @@
 import { createCommand, type AllCommands } from "./lib/commands";
-import { trashDayCollector } from "./lib/trashDayCollector";
-import * as bitmaps from "./lib/bitmaps";
-import { FontSize } from "./lib/font";
+import { fontSizeDetailsMap } from "./lib/font";
+import { generateGraphValues, getWeatherData } from "./lib/weather";
+import { rgbTo565 } from "./lib/util";
 
 export const main = async (): Promise<AllCommands[]> => {
-  const color = 0b0000011111111111;
 
-  const commands: AllCommands[] = [
-    createCommand({
-      type: 'set-state',
-      color: color,
-      fontSize: FontSize.fontSizeMd,
-      position: {
-        x: 0,
-        y: 0
-      }
-    })
-  ];
-
-  const trashInfo = await trashDayCollector();
-  if (trashInfo.trash) {
-    commands.push(createCommand({
-      type: 'bitmap',
-      size: {
-        height: 16,
-        width: 16,
-      },
-      data: [
-        0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, 0x0000, 0x0000, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, color, color, color, color, color, color, color, color, color, color, color, 0x0000, 0x0000, 0x0000,
-        0x0000, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, 0x0000, color, color, color, color, color, color, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, 0x0000, color, color, color, color, color, color, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, color, color, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, color, 0x0000, 0x0000, 0x0000,
-        0x0000, color, color, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000,
-        0x0000, color, color, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000, color, color, color, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, color, color, color, color, color, color, color, color, color, color, 0x0000, 0x0000, 0x0000, 0x0000,
-        0x0000, 0x0000, 0x0000, color, color, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-      ]
-    }))
-    commands.push(createCommand({
-      type: 'set-state',
-      position: {
-        x: 16,
-        y: 4
-      }
-    }))
-    commands.push(createCommand({
-      type: 'string',
-      value: `${trashInfo.trash.dayShort}`,
-    }))
-  }
-
-  if (trashInfo.trash && trashInfo.recycling) {
-    commands.push(createCommand({
-      type: 'set-state',
-      position: {
-        x: 0,
-        y: 17
-      }
-    }))
-  }
-
-  if (trashInfo.recycling) {
-    commands.push(createCommand({
-      type: 'string',
-      value: `Recycl ${trashInfo.recycling.dayShort}`,
-    }))
-  }
+  const commands: AllCommands[] = [];
 
   commands.push(createCommand({
-    type: 'bitmap',
+    type: 'animation',
     position: {
-      x: 1,
-      y: 33
+      x: 2,
+      y: 2
     },
-    ...bitmaps.mergeManyBitmaps({ base: bitmaps.sun, overlays: [bitmaps.cloudDark], offsetX: 0, offsetY: 0 }),
+    delay: 1000,
+    size: {
+      width: 2,
+      height: 2
+    },
+    frames: [
+      [rgbTo565(255, 0, 0), rgbTo565(255, 0, 0), rgbTo565(255, 0, 0), rgbTo565(255, 0, 0)],
+      [rgbTo565(0, 255, 0), rgbTo565(0, 255, 0), rgbTo565(0, 255, 0), rgbTo565(0, 255, 0)],
+      [rgbTo565(0, 0, 255), rgbTo565(0, 0, 255), rgbTo565(0, 0, 255), rgbTo565(0, 0, 255)],
+    ]
   }))
+
+  try {
+    const weather = await getWeatherData();
+    const graphHeight = 15;
+    const graphBottom = 62;
+    // const graphTop = graphBottom - graphHeight - 1; // -1 for padding
+    const graphLeft = 1;
+    const graphRight = graphLeft + (12 * 4); // includes 1 padding
+
+    commands.push(createCommand({
+      type: 'string',
+      value: `${Math.round(weather.daily.temperature_2m_min)}`,
+      fontSize: 'sm',
+      position: {
+        x: weather.daily.temperature_2m_min >= 100 ? graphRight : graphRight + 2,
+        y: graphBottom - fontSizeDetailsMap.sm.height + 2
+      },
+    }))
+
+    commands.push(createCommand({
+      type: 'string',
+      value: `${Math.round(weather.daily.temperature_2m_max)}`,
+      fontSize: 'sm',
+      position: {
+        x: weather.daily.temperature_2m_max >= 100 ? graphRight : graphRight + 2,
+        y: graphBottom - fontSizeDetailsMap.sm.height - fontSizeDetailsMap.sm.height
+      },
+    }))
+
+    const groupedPrec = generateGraphValues({
+      data: weather.hourly.precipitation_probability,
+      defaultMax: 100, // percent
+      defaultMin: 0,
+      graphHeight: graphHeight
+    });
+    const groupedCloud = generateGraphValues({
+      data: weather.hourly.cloud_cover,
+      defaultMax: 100, // percent
+      defaultMin: 0,
+      graphHeight: graphHeight
+    });
+    const groupedWind = generateGraphValues({
+      data: weather.hourly.wind_speed_10m,
+      defaultMax: 30, // mph
+      defaultMin: 0,
+      graphHeight: graphHeight
+    });
+
+    groupedPrec.forEach((prec, index) => {
+      const cloud = groupedCloud[index]!;
+      const wind = groupedWind[index]!;
+      const offsetStart = graphLeft + (index * 4);
+
+      commands.push(createCommand({
+        type: 'line',
+        color: rgbTo565(0, 100, 255),
+        position: {
+          x: offsetStart + 0,
+          y: 62
+        },
+        to: {
+          x: offsetStart + 0,
+          y: 62 - prec
+        }
+      }))
+      commands.push(createCommand({
+        type: 'line',
+        color: rgbTo565(255, 255, 255),
+        position: {
+          x: offsetStart + 1,
+          y: 62
+        },
+        to: {
+          x: offsetStart + 1,
+          y: 62 - cloud
+        }
+      }))
+      commands.push(createCommand({
+        type: 'line',
+        color: rgbTo565(255, 240, 50),
+        position: {
+          x: offsetStart + 2,
+          y: 62
+        },
+        to: {
+          x: offsetStart + 2,
+          y: 62 - wind
+        }
+      }))
+    })
+
+  } catch (e) {
+    console.error('Unable to generate weather data', e)
+  }
 
   return commands
 }
